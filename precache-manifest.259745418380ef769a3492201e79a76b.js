b393c8fef6e122b105890fe0d3d57b983da9d478
Jon Lai Lam <jon@lailam.co.za>
2019-04-17 12:33:25 +0200

    Updates

---
+++ precache-manifest.259745418380ef769a3492201e79a76b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/inv-dev-console/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c3c16238d1688dd2dce0",
+    "url": "/inv-dev-console/static/js/runtime~main.4f643a27.js"
+  },
+  {
+    "revision": "f0e556f4b02b35164c3a",
+    "url": "/inv-dev-console/static/js/main.a8267524.chunk.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/inv-dev-console/static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "f0e556f4b02b35164c3a",
+    "url": "/inv-dev-console/static/css/main.370620ad.chunk.css"
+  },
+  {
+    "revision": "76fd312336e61e9ef852802bfd9c0b23",
+    "url": "/inv-dev-console/index.html"
+  }
+];
